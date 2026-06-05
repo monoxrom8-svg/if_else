106d144cab0877c04cc347cb33facf620fee0035
xosnfis <tasuev2007@gmail.com>
2026-06-06 00:20:46 +0500

    podpis

--- tramplin/static/tramplin/leaflet-tiles.js
+++ tramplin/static/tramplin/leaflet-tiles.js
@@ -1,11 +1,13 @@
 /** Общий слой тайлов для всех карт Трамплин (стабильнее старого *.tile.openstreetmap.org). */
-window.TRAMPLIN_addTileLayer = function (map) {
+window.TRAMPLIN_addTileLayer = function (map, options) {
   if (!map || typeof L === "undefined") return null;
+  options = options || {};
   return L.tileLayer(
     "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
     {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
+      attribution: options.hideAttribution
+        ? ""
+        : '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
       subdomains: "abcd",
       maxZoom: 20,
     }
